a0d97ba41b6b5076863fd77e98279d38cf429991
jae-hun-e <wognskec@hanyang.ac.kr>
2022-08-07 23:03:05 +0900

    change from REST API to GraphQL

--- resolvers.js
+++ resolvers.js
@@ -1,52 +1,61 @@
-import {tweets, users} from "./dummyData.js";
-
-
+import { tweets, users } from "./dummyData.js";
+import fetch from "node-fetch";
 // Get /text
 // Get /age
 export const resolvers = {
-  Query : {
-    allTweets(){
-      console.log("allTweets called")
-      return tweets
+  Query: {
+    allTweets() {
+      console.log("allTweets called");
+      return tweets;
     },
-    tweet( __ , {id}){
-      console.log("tweet called")
-      return tweets.find(data => data.id === id)
+    tweet(__, { id }) {
+      console.log("tweet called");
+      return tweets.find((data) => data.id === id);
     },
-    allUsers(){
-      console.log("allUsers called")
-      return users
-    }
+    allUsers() {
+      console.log("allUsers called");
+      return users;
+    },
+    allMovies() {
+      console.log("allMovies called");
+      const data = fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+      console.log(data);
+      return data;
+    },
   },
   Mutation: {
-    postTweet(__, {text, userId}){
+    postTweet(__, { text, userId }) {
       const newTweet = {
-        id : (tweets.length +1).toString(),
+        id: (tweets.length + 1).toString(),
         text,
-      }
-      tweets.push(newTweet)
-      return newTweet
+      };
+      tweets.push(newTweet);
+      return newTweet;
     },
-    deleteTweet(__, {id}){
-      const delTweet = tweets.find(tweet => tweet.id === id)
-      if(!delTweet) return false
+    deleteTweet(__, { id }) {
+      const delTweet = tweets.find((tweet) => tweet.id === id);
+      if (!delTweet) return false;
       //TODO 만약 JS로 delete를 구현한다면 recoil, contextAPI를 이용한 전영상태관리가 필요
-      tweets = tweets.filter(tweet => tweet !== id)
-      return true
-    }
+      tweets = tweets.filter((tweet) => tweet !== id);
+      return true;
+    },
   },
-  User:{
-    fullName({firstName, lastName}){ // root args = users Obj
+  User: {
+    fullName({ firstName, lastName }) {
+      // root args = users Obj
       // console.log(firstName, lastName) // fullName을 호출하는 User를 보게된다
       // console.log("fullName")
-      return `${firstName}${lastName}`
-    }
+      return `${firstName}${lastName}`;
+    },
   },
   Tweet: {
-    author(root){ // root args = tweets Obj
-      console.log(root)
-      console.log("tweet resolver called")
-      return users.find((user) => user.id === root.userId)
-    }
-  }
-}
+    author(root) {
+      // root args = tweets Obj
+      console.log(root);
+      console.log("tweet resolver called");
+      return users.find((user) => user.id === root.userId);
+    },
+  },
+};
--- schema.js
+++ schema.js
@@ -3,33 +3,106 @@ import {gql} from "apollo-server";
 export const typeDefs = gql`
     
   type Query {
-  allUsers: [User!]!
-  allTweets: [Tweet]
-  tweet(id: ID): Tweet
-  ping : String
+      allUsers: [User!]!
+      allTweets: [Tweet]
+      tweet(id: ID): Tweet
+      ping : String
+      allMovies: [Movie!]!
   }
-  
+
+  """
+  TEST
+  """
   type Mutation {
-  postTweet(text: String!, userId: ID!): Tweet!
-  deleteTweet(id: ID!): Boolean!
+      postTweet(text: String!, userId: ID!): Tweet!
+      deleteTweet(id: ID!): Boolean!
   }
   
   type User {
-  id: ID!
-  firstName: String!
-  lastName: String!
-  """
-  fullName = FirstName + LastName
-  """
-  fullName: String!
-  }
+      id: ID!
+      firstName: String!
+      lastName: String!
+      """
+      fullName = FirstName + LastName
+      """
+      fullName: String!
+    }
   
   """
   Tweet Schema 구조
   """
   type Tweet {
-  id: ID
-  text: String
-  author: User
+      id: ID
+      text: String
+      author: User
+  }
+  
+  type Torrents {
+      url : String! 
+      hash : String! 
+      quality : String! 
+      type : String! 
+      seeds : Int!
+      peers : Int!
+      size : String! 
+      size_bytes : Int!
+      date_uploaded : String! 
+      date_uploaded_unix : Int!
+  }
+  
+  type Movie {
+      id : Int! 
+      url : String! 
+      imdb_code : String! 
+      title : String! 
+      title_english : String! 
+      title_long : String! 
+      slug : String! 
+      year : Int! 
+      rating : Int! 
+      runtime : Float! 
+      genres : [String!]! 
+      summary : String! 
+      description_full : String! 
+      synopsis : String! 
+      yt_trailer_code : String! 
+      language : String! 
+      mpa_rating : String! 
+      background_image : String! 
+      background_image_original : String! 
+      small_cover_image : String! 
+      medium_cover_image : String! 
+      large_cover_image : String! 
+      state : String! 
+      torrents : [Torrents!]!
+      date_uploaded : String! 
+      date_uploaded_unix : Int! 
+      
   }
 `;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
